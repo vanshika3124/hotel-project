2c463652e44010683208cb2c2b4867f322cbc5d3
Vanshika Agrawal <vanshika310.ghaziabad@gmail.com>
2026-05-08 23:06:25 +0530

    fixed errors

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc'
+import react from '@vitejs/plugin-react' // Yahan SWC ki jagah simple react plugin use karein
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
