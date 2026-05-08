310395d418a48440a475620c1b5aa2c2f3ce1e5c
Vanshika Agrawal <vanshika310.ghaziabad@gmail.com>
2026-05-08 23:04:06 +0530

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
